f93bbdfb62482a42e795239eda95380f9ecf30d8
aLaclair <60781019+aLaclair@users.noreply.github.com>
2020-07-22 16:17:44 -0400

    better filtering and post route declared

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const path = require('path')
 const PORT = 8080
 const app = express()
+const parse = require('body-parser')
+
+app.use(parse.urlencoded({ extended: true }))
+app.use(parse.json())
 
 app.use(express.static(path.join(__dirname, './app/public')))
 
